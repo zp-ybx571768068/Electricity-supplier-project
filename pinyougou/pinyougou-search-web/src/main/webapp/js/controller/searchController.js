4453a9131799db4a6b8f5ba96fd0960ff7686d16
zp_ybx <ybx571768068@gmail.com>
2018-12-25 15:32:07 +0800

    多关键字搜索空格处理

--- pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -1,38 +1,111 @@
 app.controller("searchController", function ($scope, searchService) {
 
+    //搜索
+    $scope.search = function () {
+        searchService.search($scope.searchMap).success(function (response) {
+            $scope.resultMap = response;
+            //构建页面分页导航条信息
+            buildPageInfo();
+        });
+    };
+
     //搜索条件对象
-    $scope.searchMap = {"keywords":"","category":"","brand":"","spec":{},"price":""};
+    $scope.searchMap = {"keywords":"","category":"","brand":"","spec":{},"price":"","pageNo":1,"pageSize":40};
 
     //过滤查询
     $scope.addSearchItem = function(key,value){
-        if ("brand" == key || "category" == key){
+        if ("brand" == key || "category" == key || "price" == key){
             //如果点击的是品牌或者分类
             $scope.searchMap[key] = value;
         } else {
             //如果是规格
             $scope.searchMap.spec[key] = value;
         }
+        $scope.searchMap.pageNo=1;
         //点击过滤条件之后需要重新搜索
         $scope.search();
     };
     //撤销过滤条件
     $scope.removeSearchItem = function(key){
-        if ("brand" == key || "category" == key){
+        if ("brand" == key || "category" == key || "price" == key){
             //如果点击的是品牌或者分类
             $scope.searchMap[key] ='';
         } else {
             //如果是规格
             delete $scope.searchMap.spec[key];
         }
+        $scope.searchMap.pageNo=1;
         //点击过滤条件之后需要重新搜索
         $scope.search();
     };
-    //搜索
-    $scope.search = function () {
-        searchService.search($scope.searchMap).success(function (response) {
-            $scope.resultMap = response;
 
-        });
+    //构建页面分页导航条信息
+    buildPageInfo = function () {
+        //定义要再页面显示的页号集合
+        $scope.pageNoList = [];
+
+        //定义要在页面显示的页号数量
+        var showPageNoTotal = 5;
+
+        //起始页号
+        var startPageNo = 1;
+        //结束页号
+        var endPageNo = $scope.resultMap.totalPages;
+
+        //如果总页数大于要显示的页数才有需要处理显示页号数；否则直接显示所有页号
+        if ($scope.resultMap.totalPages > showPageNoTotal){
+
+            //计算当前页的左右间隔
+            var interval = Math.floor(showPageNoTotal / 2);
+
+            //根据间隔得出起始、结束页号
+            startPageNo = parseInt($scope.searchMap.pageNo)-interval;
+            endPageNo = parseInt($scope.searchMap.pageNo) + interval;
+
+            //处理页号越界
+            if (startPageNo >= 1){
+                // 如果结束页号是大于总页数的则都设置为总页数，起始页号就
+                if (endPageNo > $scope.resultMap.totalPages){
+                    startPageNo = $scope.resultMap.totalPages-showPageNoTotal+1;
+                    endPageNo = $scope.resultMap.totalPages
+                }
+            }else {
+                // 如果起始页号是小于1的则都设置为1，结束页号就为要显示的
+                startPageNo = 1;
+                endPageNo = showPageNoTotal;
+            }
+        }
+
+        //分页导航条上前后三个点
+        $scope.frontDot = false;
+        if (startPageNo > 1){
+            $scope.frontDot = true;
+        }
+
+        $scope.backDot = false;
+        if (endPageNo > $scope.resultMap.totalPages) {
+            $scope.backDot = true;
+        }
+
+        //设置要显示的页号
+        for (var i = startPageNo;i <= endPageNo;i++){
+            $scope.pageNoList.push(i);
+        }
+        alert($scope.pageNoList)
     };
+
+    //判断是否为当前页
+    $scope.isCurrentPage=function (pageNo) {
+       return $scope.searchMap.pageNo == pageNo;
+    };
+
+    //根据页号查询
+    $scope.queryByPageNo = function (pageNo) {
+        pageNo = parseInt(pageNo);
+        if (pageNo >0 && pageNo <= $scope.resultMap.totalPages) {
+            $scope.searchMap.pageNo = pageNo;
+            $scope.search();
+        }
+    }
 
 });
--- pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -1,14 +1,13 @@
 package com.pinyougou.search.service.impl;
 
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.dubbo.config.annotation.Service;
+import com.ctc.wstx.util.StringUtil;
 import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.HighlightOptions;
-import org.springframework.data.solr.core.query.SimpleHighlightQuery;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
 import org.springframework.data.solr.core.query.result.HighlightEntry;
 import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
@@ -16,6 +15,7 @@ import org.springframework.data.solr.core.query.result.ScoredPage;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author zp
@@ -28,12 +28,20 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 
     /**
      * 根据搜索关键字和其它条件到solr中查询数据
+     *
      * @param searchMap 搜索条件
      * @return 查询结果
      */
     @Override
     public Map<String, Object> search(Map<String, Object> searchMap) {
-        Map<String,Object> requestMap = new HashMap<>();
+        Map<String, Object> requestMap = new HashMap<>();
+
+
+        //处理搜索关键字中的空格问题
+        if (!StringUtils.isEmpty(searchMap.get("keywords").toString())) {
+            searchMap.put("keywords",searchMap.get("keywords").toString().replaceAll(" ",""));
+        }
+
 
         //创建高亮搜索对象
         SimpleHighlightQuery query = new SimpleHighlightQuery();
@@ -42,38 +50,94 @@ public class ItemSearchServiceImpl implements ItemSearchService {
         Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
         query.addCriteria(criteria);
 
-        //设置高亮
-        HighlightOptions highlightOptions = new HighlightOptions();
-        //高亮域
-        highlightOptions.addField("item_title");
-        //高亮起始标签
-        highlightOptions.setSimplePrefix("<em style='color:red'>");
-        //高亮标签结束
-        highlightOptions.setSimplePostfix("</em>");
-        query.setHighlightOptions(highlightOptions);
-
-        //查询
-        HighlightPage<TbItem> itemHighlightPage = solrTemplate.queryForHighlightPage(query, TbItem.class);
-
-        //处理高亮标题
-        List<HighlightEntry<TbItem>> highlighted = itemHighlightPage.getHighlighted();
-        if (highlighted != null && highlighted.size() >0 ){
-
-            for (HighlightEntry<TbItem> entry : highlighted){
-                //获取高亮的列表
-                List<HighlightEntry.Highlight> highlights = entry.getHighlights();
-
-                if (highlights != null && highlights.size() >0 && highlights.get(0).getSnipplets() != null){
-                    //第一个get（0）为获取第一个域，第二个get（0）为获取改域的第一个高亮字符串
-                    String title = highlights.get(0).getSnipplets().get(0).toString();
-                    //设置高亮标题
-                    entry.getEntity().setTitle(title);
-                }
+        //按照分类过滤
+        if (!StringUtils.isEmpty(searchMap.get("category").toString())) {
+            Criteria categoryCriteria = new Criteria("item_category").is(searchMap.get("category"));
+            SimpleFilterQuery simpleFilterQuery = new SimpleFilterQuery(categoryCriteria);
+            query.addFilterQuery(simpleFilterQuery);
+        }
+        //按照品牌过滤
+        if (!StringUtils.isEmpty(searchMap.get("brand").toString())) {
+            Criteria brandCriteria = new Criteria("item_brand").is(searchMap.get("brand"));
+            SimpleFilterQuery simpleFilterQuery = new SimpleFilterQuery(brandCriteria);
+            query.addFilterQuery(simpleFilterQuery);
+        }
+        //按照规格过滤
+        if (searchMap.get("spec") != null) {
+            Map<String, String> specMap = (Map<String, String>) searchMap.get("spec");
+            Set<Map.Entry<String, String>> entries = specMap.entrySet();
+            for (Map.Entry<String, String> entry : entries) {
+                Criteria specCriteria = new Criteria("item_spec_"+ entry.getKey()).is(entry.getValue());
+                SimpleFilterQuery simpleFilterQuery = new SimpleFilterQuery(specCriteria);
+                query.addFilterQuery(simpleFilterQuery);
             }
         }
-        //设置返回的商品列表
-        requestMap.put("rows",itemHighlightPage.getContent());
 
-        return requestMap;
+        //按照价格区间过滤
+        if (!StringUtils.isEmpty(searchMap.get("price").toString())){
+            //获取起始、结束价格
+            String[] prices = searchMap.get("price").toString().split("-");
+
+            //价格大于或等于起始价格
+            Criteria startPriceCriteria = new Criteria("item_price").greaterThan(prices[0]);
+            SimpleFilterQuery startPriceFilterQuery = new SimpleFilterQuery(startPriceCriteria);
+            query.addFilterQuery(startPriceFilterQuery);
+
+            //价格小于等于结束价格
+            if (!"*".equals(prices[1])){
+                Criteria endPriceCriteria = new Criteria("item_price").lessThan(prices[1]);
+                SimpleFilterQuery endPriceFilterQuery = new SimpleFilterQuery(endPriceCriteria);
+                query.addFilterQuery(endPriceFilterQuery);
+            }
+        }
+        //设置分页信息
+        Integer pageNo = 1;
+        if (!StringUtils.isEmpty(searchMap.get("pageNo").toString())){
+            pageNo = Integer.parseInt(searchMap.get("pageNo").toString());
+        }
+        Integer pageSize = 20;
+        if (!StringUtils.isEmpty(searchMap.get("pageSize").toString())){
+            pageSize = Integer.parseInt(searchMap.get("pageSize").toString());
+        }
+        query.setOffset((pageNo-1)*pageSize);
+        query.setRows(pageSize);
+
+
+            //设置高亮
+            HighlightOptions highlightOptions = new HighlightOptions();
+            //高亮域
+            highlightOptions.addField("item_title");
+            //高亮起始标签
+            highlightOptions.setSimplePrefix("<em style='color:red'>");
+            //高亮标签结束
+            highlightOptions.setSimplePostfix("</em>");
+            query.setHighlightOptions(highlightOptions);
+
+            //查询
+            HighlightPage<TbItem> itemHighlightPage = solrTemplate.queryForHighlightPage(query, TbItem.class);
+
+            //处理高亮标题
+            List<HighlightEntry<TbItem>> highlighted = itemHighlightPage.getHighlighted();
+            if (highlighted != null && highlighted.size() > 0) {
+
+                for (HighlightEntry<TbItem> entry : highlighted) {
+                    //获取高亮的列表
+                    List<HighlightEntry.Highlight> highlights = entry.getHighlights();
+
+                    if (highlights != null && highlights.size() > 0 && highlights.get(0).getSnipplets() != null) {
+                        //第一个get（0）为获取第一个域，第二个get（0）为获取改域的第一个高亮字符串
+                        String title = highlights.get(0).getSnipplets().get(0).toString();
+                        //设置高亮标题
+                        entry.getEntity().setTitle(title);
+                    }
+                }
+            }
+            //设置返回的商品列表
+            requestMap.put("rows", itemHighlightPage.getContent());
+            requestMap.put("totalPages",itemHighlightPage.getTotalPages());
+            requestMap.put("total",itemHighlightPage.getTotalElements());
+
+            return requestMap;
+        }
     }
-}
+
